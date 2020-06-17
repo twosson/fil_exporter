c4adf75c078ecbaed30188ac6a6a05142848ffc0
twosson <twosson@163.com>
2020-06-18 01:06:31 +0800

    增加build工具，规范开发，发布流程。

--- build.go
+++ build.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -16,6 +19,122 @@ var (
 	LD_FLAGS   = fmt.Sprintf("-X \"main.buildTime=%s\" -X main.gitCommit=%s", BUILD_TIME, GIT_COMMIT)
 	GO_FLAGS   = fmt.Sprintf("-ldflags=%s", LD_FLAGS)
 )
+
+func main() {
+	flag.Parse()
+	for _, cmd := range flag.Args() {
+		switch cmd {
+		case "ci":
+			test()
+			vet()
+		case "vet":
+			vet()
+		case "test":
+			test()
+		case "go-install":
+			goInstall()
+		case "clean":
+			clean()
+		case "generate":
+			generate()
+		case "build":
+			build()
+		case "run-dev":
+			runDev()
+		case "version":
+			version()
+		case "release":
+			release()
+		default:
+			log.Fatalf("Unknown command %q", cmd)
+		}
+	}
+}
+
+func vet() {
+	runCmd("go", nil, "vet", "./collector/...")
+}
+
+func test() {
+	runCmd("go", nil, "test", "-v", "./collector/...")
+}
+
+func clean() {
+
+}
+
+func goInstall() {
+	pkgs := []string{
+		"github.com/golang/mock/gomock",
+		"github.com/golang/mock/mockgen",
+		//"github.com/golang/protobuf/protoc-gen-go",
+	}
+	for _, pkg := range pkgs {
+		runCmd("go", map[string]string{"GO111MODULE": "on"}, "install", pkg)
+	}
+}
+
+func generate() {
+	removeFakes()
+	runCmd("go", nil, "generate", "-v", "./collector/...")
+}
+
+func build() {
+	newPath := filepath.Join(".", "build")
+	os.MkdirAll(newPath, 0755)
+
+	linuxEnvs := map[string]string{
+		"CGO_ENABLED": "0",
+		"GOOS":        "linux",
+		"GOARCH":      "amd64",
+	}
+
+	artifact := "fil_exporter"
+	runCmd("go", linuxEnvs, "build", "-o", "build/"+artifact, GO_FLAGS, "-v", "./cmd")
+}
+
+func runDev() {
+	env := make(map[string]string)
+	for _, e := range os.Environ() {
+		parts := strings.SplitN(e, "=", 2)
+		env[parts[0]] = parts[1]
+	}
+	runCmd("build/fil_exporter", env)
+}
+
+func version() {
+	fmt.Println(VERSION)
+}
+
+func release() {
+	runCmd("git", nil, "tag", "-a", VERSION, "-m", fmt.Sprintf("\"Release %s\"", VERSION))
+	runCmd("git", nil, "push", "--follow-tags")
+}
+
+func removeFakes() {
+	checkDirs := []string{"collector"}
+	fakePaths := []string{}
+
+	for _, dir := range checkDirs {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if !info.IsDir() {
+				return nil
+			}
+			if info.Name() == "fake" {
+				fakePaths = append(fakePaths, filepath.Join(path, info.Name()))
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatalf("generate (%s): %s", dir, err)
+		}
+	}
+
+	log.Print("Removing fakes from collector/")
+	for _, p := range fakePaths {
+		os.RemoveAll(p)
+	}
+}
 
 func newCmd(command string, env map[string]string, args ...string) *exec.Cmd {
 	realCommand, err := exec.LookPath(command)
